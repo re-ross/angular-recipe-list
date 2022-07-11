8dfd2bc6ef3751fcda2793a1cae85c100685beb5
Ryan Ross <ryan.ross@meyerdistributing.com>
2022-07-11 08:54:11 -0500

    styled header component and created Recipe model class

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,16 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is just a test',
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459396-960-720.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
